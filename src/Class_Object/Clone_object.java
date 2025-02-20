1b9c41b57572456dec786657b6517ef5a3408d45
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-02-20 08:24:13 +0530

    programs

---
+++ src/Class_Object/Clone_object.java
@@ -0,0 +1,28 @@
+package Class_Object;
+
+//by implementing cloneable interface we can create clone object of class;
+
+public class Clone_object implements Cloneable {
+
+    int x;
+    public Clone_object(int x)
+    {
+        this.x=x;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Clone_object t1=new Clone_object(20);
+
+        //syntax:-
+        //className object2=(className)object1.clone();
+        Clone_object t2 =(Clone_object)t1.clone();
+
+        System.out.println(t2.getX());
+        System.out.println(t2.hashCode());
+
+    }
+}
+
--- src/Class_Object/Newinstance.java
+++ src/Class_Object/Newinstance.java
@@ -4,6 +4,7 @@ package Class_Object;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+/*
 public class Newinstance {
 
     String name;
@@ -29,4 +30,34 @@ public class Newinstance {
 
         }
     }
+}*/
+
+
+public class Newinstance {
+
+    private int age;
+
+    public Newinstance(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            //syntax
+            //Constructor<className> obj1=className.class.getConstructor(String.class,int.class,float.class);
+            //className obj2=obj1.newInstance("parameter");
+
+            Constructor<Newinstance> constructor = Newinstance.class.getConstructor(int.class);
+            Newinstance instance = constructor.newInstance(10);
+            System.out.println(instance.getAge());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
