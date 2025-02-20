24af9ec5643b417eb6be514df22e6e8a4df97651
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-02-20 10:55:05 +0530

    Deserialisation of Object

---
+++ src/Class_Object/Deserialisation.java
@@ -0,0 +1,27 @@
+package Class_Object;
+
+import java.io.*;
+
+public class Deserialisation implements Serializable {
+
+    String name;
+    public Deserialisation(String name)
+    {
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Deserialisation t1=new Deserialisation("Krish Patel");
+
+//        ObjectOutputStream ser =new ObjectOutputStream(new FileOutputStream("student.ser"));
+//        ser.writeObject(t1);
+//        ser.close();
+
+ObjectInputStream dser=new ObjectInputStream(new FileInputStream("student.ser"));
+Deserialisation obj=(Deserialisation)dser.readObject();
+        System.out.println(obj.getName());
+    }
+}
