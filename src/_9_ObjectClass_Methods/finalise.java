e36e3863156c2b060af227a712b343c4082147e1
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-02-21 10:48:27 +0530

    Object class Methods in java

---
+++ src/_9_ObjectClass_Methods/finalise.java
@@ -0,0 +1,31 @@
+package _9_ObjectClass_Methods;
+
+public class finalise extends Object{
+
+    finalise()
+    {
+        System.out.println("object is created");
+    }
+
+    @Override
+    public void finalize()//override finalize method for cleanup code
+    {
+        System.out.println("object is destroyed");
+    }
+    public static void main(String[] args) {
+
+        finalise t1=new finalise();
+        t1=null;// 1.. by making reference or object to null
+
+        //2.. by assigning object or another object
+        // here t2 is assigned to t1 and t1 will be destroyed
+        finalise t2=new finalise();
+        finalise t3=new finalise();
+        t2=t3;
+
+
+        new finalise();//by creating anonymous object
+        System.gc();// this method will force garbage collector to to garbage collection
+
+    }
+}
