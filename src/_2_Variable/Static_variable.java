547ebb4b24b6c7d44e2b53454a7e1c82a35d772c
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-02-22 15:44:51 +0530

    formating

--- src/_2_Variable/Static_variable.java
+++ src/_2_Variable/Static_variable.java
@@ -3,47 +3,47 @@ package _2_Variable;
 public class Static_variable {
 
     //static variables are created at once any shared among multiple objects of class
-    public static String name="krishna";
-    public static int number=14;
+    public static String name = "krishna";
+    public static int number = 14;
     public static String college;
 
     //constructor of class
-    public Static_variable()
-    {
+    public Static_variable() {
         //executed second after initial block
-        name="krish patel";
+        name = "krish patel";
         System.out.println(name);
 
     }
 
-    public static void hello(){
+    public static void hello() {
 //executed after constructor
         //this will override the first name
-        Static_variable.name="priya";
+        Static_variable.name = "priya";
         System.out.println(Static_variable.name);
 
         //value of variable can be set using class name in constructor and methods and blocks
-        Static_variable.number=23;
+//        Static_variable.number = 23;
         System.out.println(number);
     }
 
     {
         //initial block
 //        executed first
-        Static_variable.name="kalpesh";
+        Static_variable.name = "kalpesh";
         System.out.println(Static_variable.name);
 
-        Static_variable.number=19;
+        Static_variable.number = 19;
     }
-    public static void main(String[] args) {
-        Static_variable obj=new Static_variable();
 
-        Static_variable.hello();
+    public static void main(String[] args) {
+        Static_variable obj = new Static_variable();
+
+        obj.hello();
 
         System.out.println(Static_variable.name);
         System.out.println(number);
 
-        Static_variable.college="AIET";
+        Static_variable.college = "AIET";
         System.out.println(college);
 
         //values are changed based on property of execution of blocks and components of class
