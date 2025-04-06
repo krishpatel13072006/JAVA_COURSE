d4e7eda7c30e6de24697433c1948546ed714da5e
krishpatel13072006 <krishpatelhacker.13579@gmail.com>
2025-04-06 18:02:33 +0530

    Array programs in java

---
+++ src/Palindrome_array.java
@@ -0,0 +1,30 @@
+public class Palindrome_array {
+
+    public static boolean reverse(int arr[])
+    {
+        int start=0;
+        int last=arr.length-1;
+
+        for (int i = 0; i < arr.length/2; i++) {
+
+            if (arr[start] != arr[last])
+            {
+                return false;
+            }
+            else
+            {
+                start++;
+                last--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int numbers[]={12,23,23,12};
+
+        boolean x=reverse(numbers);
+        System.out.println(x);
+    }
+}
---
+++ src/sub_arrays.java
@@ -0,0 +1,21 @@
+public class sub_arrays {
+public static void sub_arr(int arr[])
+{
+    for (int i = 0; i < arr.length; i++) {
+
+        for (int j = i+1; j < arr.length; j++) {
+
+            for (int k = i; k <= j ; k++) {
+                System.out.print(arr[k]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+    public static void main(String[] args) {
+        int num[]={1,3,5,7,9};
+        sub_arr(num);
+    }
+}
